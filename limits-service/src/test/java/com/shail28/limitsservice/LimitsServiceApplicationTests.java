ecf31d1cdc080549f1e157342cc515df5aabd50b
shailendra <shailendra.itpro@gmail.com>
2021-03-14 02:27:24 +0530

    new limits-service/.gitignore

---
+++ limits-service/src/test/java/com/shail28/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shail28.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
