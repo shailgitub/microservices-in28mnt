35f36966034354e84f90e976e383c76d55ab354c
shailendra <shailendra.itpro@gmail.com>
2021-03-16 18:49:59 +0530

    commited eureka changes

---
+++ currency-exchange-service/src/main/java/com/shail28/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.shail28.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	ExchangeValue findByFromAndTo(String from, String to);
+}
