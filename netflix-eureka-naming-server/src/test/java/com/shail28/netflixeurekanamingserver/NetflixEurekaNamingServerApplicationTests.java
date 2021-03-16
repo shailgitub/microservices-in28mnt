dcb170028c19305aad0b5b1ab8a3221e772613aa
shailendra <shailendra.itpro@gmail.com>
2021-03-16 18:46:22 +0530

    naming server created

---
+++ netflix-eureka-naming-server/src/test/java/com/shail28/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shail28.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
